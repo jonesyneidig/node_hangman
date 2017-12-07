1e105ef5a29c40a307e932757a1ddd2b2595622e
John Neidig Jr <jonesyneidig@Johns-MacBook-Pro-8.local>
2017-12-07 09:41:45 -0600

    intial

---
+++ game.js
@@ -0,0 +1,5 @@
+//list of words to guess.
+exports.newWord = {
+  wordList:['ROYALS','CARDINALS','WHITE SOX','ROCKIES','DODGERS','YANKESS','RED SOX','MARLINS','BREWERS','BRAVES','TWINS','ANGELS','DIAMONDBACKS'],
+  hangman: ["\n\n\n\n\n\n\n-------------", "\n |\n |\n |\n |\n |\n |\n |\n-------------", "____________\n |         |\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        |\n |\n-------------","____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"]
+};
